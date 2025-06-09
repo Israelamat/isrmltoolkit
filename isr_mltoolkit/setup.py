83c915842cec6abf95c794698d82d27746461977
Israelamat <israelamatgil@gmail.com>
2025-06-09 02:57:08 +0200

    version2

--- isr_mltoolkit/setup.py
+++ isr_mltoolkit/setup.py
@@ -1,25 +1,22 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='isrmltoolkit',  # Nombre que usará pip install
-    version='0.1.5', 
+    name='isrmltoolkit',
+    version='0.1.5',
+    packages=find_packages(),  # <--- Esto encuentra automáticamente isrmltoolkit/
+    install_requires=[
+        'pandas',
+        'matplotlib',
+        'seaborn',
+        'scikit-learn'
+    ],
     author='Isra',
     author_email='ismael@email.com',
     description='Funciones y utilidades útiles para proyectos de Machine Learning',
-    long_description=open('README.md', encoding='utf-8').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/Israelamat/pip_install',  # Actualízalo si es necesario
-    packages=find_packages(),
-    install_requires=[
-        'pandas',
-        'scikit-learn',
-        'matplotlib',
-        'seaborn'
-    ],
+    url='https://github.com/Israelamat/pip_install',
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.6',
 )
